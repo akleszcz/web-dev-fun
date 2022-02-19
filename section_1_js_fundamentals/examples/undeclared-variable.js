8a76e52a6c3eb5ea0326f1a0b4a9af04ec9938ae
Anna Kleszcz <anna.kleszcz123@gmail.com>
2022-02-19 11:41:02 +0100

    File structure adjusted

---
+++ section_1_js_fundamentals/examples/undeclared-variable.js
@@ -0,0 +1,8 @@
+/*
+ Run it with
+ 'node path_to_this_file', e.g.
+ 'node section_1_js_fundamentals\\examples\\undeclared-variable.js' on Windows
+*/
+x = 5;
+console.log(x); // 5
+console.log(global.x); // 5
--- src/undeclared-variable.js
+++
@@ -1,8 +0,0 @@
-/*
- Run it with
- 'node path\to\this\file', e.g.
- 'node src\undeclared-variable.js' on Windows
-*/
-x = 5;
-console.log(x); // 5
-console.log(global.x); // 5
